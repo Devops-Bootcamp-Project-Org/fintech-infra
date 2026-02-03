77358b2243a4320f8a2c14e05bdbe6957b52d295
Shollz <augustineogunlade1@gmail.com>
2026-02-03 22:36:03 +0000

    updated acm

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -14,7 +14,7 @@ resource "aws_acm_certificate" "fusisoft_cert_cert" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.fusisoft_cert_cert.domain_validation_options :
+    for dvo in aws_acm_certificate.fusisoft_cert.domain_validation_options :
     dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
@@ -34,6 +34,6 @@ resource "aws_route53_record" "cert_validation" {
 
 
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.fusisoft_cert_cert.arn
+  certificate_arn         = aws_acm_certificate.fusisoft_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
