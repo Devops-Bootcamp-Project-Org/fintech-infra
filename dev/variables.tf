37646cc32d00c74227dc0185bccf36296aa481c5
Shollz <augustineogunlade1@gmail.com>
2026-02-04 03:02:46 +0000

    Final eks cluster name

--- dev/variables.tf
+++ dev/variables.tf
@@ -46,7 +46,7 @@ variable "tags" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "dev-fintech-groupb"
+  default     = "dev-fintek-cluster"
 }
 
 variable "rolearn" {
