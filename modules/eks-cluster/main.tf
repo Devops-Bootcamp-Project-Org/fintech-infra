cd69475f81a3242676df6bc6553462267ba8c070
Shollz <augustineogunlade1@gmail.com>
2026-02-04 02:25:08 +0000

    Final updated eks cluster

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -14,7 +14,8 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    # Added --region to ensure the token is generated for the right place
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
   }
 }
 
